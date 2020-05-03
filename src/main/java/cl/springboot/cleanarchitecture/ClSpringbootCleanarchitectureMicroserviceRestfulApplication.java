e09346aabfbfa48c053654921f74a24040f79972
CristianHume <53346632+cristianhume@users.noreply.github.com>
2020-05-02 22:19:50 -0400

    [feature/CREATE-PROJECT]: Create a Spring Boot (JAVA) project
    * Generated project with Spring Initializer https://start.spring.io/
    
    Signed-off-by: CristianHume <53346632+cristianhume@users.noreply.github.com>

---
+++ src/main/java/cl/springboot/cleanarchitecture/ClSpringbootCleanarchitectureMicroserviceRestfulApplication.java
@@ -0,0 +1,13 @@
+package cl.springboot.cleanarchitecture;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClSpringbootCleanarchitectureMicroserviceRestfulApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClSpringbootCleanarchitectureMicroserviceRestfulApplication.class, args);
+	}
+
+}
